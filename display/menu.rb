65b42ea2141335943aec1a2a84d5c5f9d70e72c7
Geovanny Neves Liberato <geovanny.liberato@inventsoftware.com.br>
2026-02-24 17:45:53 -0300

    melhorias

--- display/menu.rb
+++ display/menu.rb
@@ -1,16 +1,16 @@
 def iniciar_menu(produtos = [])
 	loop do
 		mensagem_amarelo("================= Escolha uma das opções abaixo =================", false, false)
-		mensagem_azul("1 - Cadastro de produtos", false, false)
-		mensagem_azul("2 - List de produtos", false, false)
-		mensagem_azul("3 - Retirada do Estoque", false, false)
-		mensagem_azul("4 - Sair", false, false)
+		mensagem_azul("#{vermelho("1")} - Cadastro de produtos", false, false)
+		mensagem_azul("#{vermelho("2")} - List de produtos", false, false)
+		mensagem_azul("#{vermelho("3")} - Retirada do Estoque", false, false)
+		mensagem_azul("#{vermelho("4")} - Sair", false, false)
 
 		opcao = gets.to_i
 
 		case opcao
 		when 1
-			cadastro_produto(produtos)
+			cadastrar_produto(produtos)
 		when 2
 			mensagem "Lista"
 		when 3
